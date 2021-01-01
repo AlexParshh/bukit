e9503b0ebce1ecb4f89e113d66bb515eb0871248
Alexparshh <alexparshh@gmail.com>
2021-01-01 13:27:20 -0500

    setup mongodb database

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+
+app.use(
+    bodyParser.urlencoded({
+      extended: false
+    })
+  );
+  app.use(bodyParser.json());
+
+
+// db info
+const db = require("./config/keys").mongoURI;
+
+
+// mongodb connection
+mongoose.connect(db,
+    {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+const port = 5000; 
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+
+
