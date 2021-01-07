213660a65eae998fca43386f5689e951a62f319e
Alexparshh <alexparshh@gmail.com>
2021-01-07 16:30:06 -0500

    rebuilt landing & navbar with react-bootstrap

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,85 +3,61 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import { getItems } from "../../actions/userListActions";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import "./dashboard.css";
 
 class Dashboard extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
-      items: this.props.userList.userList
-    }
+      items: this.props.userList.userList,
+    };
 
     this.props.getItems();
-
   }
 
-  onLogoutClick = e => {
+  onLogoutClick = (e) => {
     e.preventDefault();
     this.props.logoutUser();
   };
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.userList != this.props.userList) {
-      this.setState({userList:nextProps.userList})
+      this.setState({ userList: nextProps.userList });
     }
   }
 
-
-  
   render() {
-    
     const { user } = this.props.auth;
 
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                Welcome to {" "}
-                <span style={{ fontFamily: "monospace" }}>Bukit</span> 👏
-              </p>
-            </h4>
-
-            <h1></h1>
-      
-
-
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
-          </div>
-        </div>
-      </div>
+        <Container id="container">
+          <Row id="middle-element">
+            <Col>
+              <h4>
+                <b>Hey there, {user.name.split(" ")[0]}</b>
+              </h4>
+            </Col>
+          </Row>
+        </Container>
     );
   }
 }
 
 Dashboard.propTypes = {
-  logoutUser: PropTypes.func.isRequired,
   getItems: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  userList: PropTypes.object.isRequired
+  userList: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth,
-  userList: state.userList
+  userList: state.userList,
 });
 
-export default connect(
-  mapStateToProps,
-  { logoutUser, getItems }
-)(Dashboard);
+export default connect(mapStateToProps, { getItems })(Dashboard);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,38 +1,53 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/esm/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Navbar from "react-bootstrap/Navbar";
+import { logoutUser } from "../../actions/authActions";
+import Button from "react-bootstrap/Button";
 
-class Navbar extends Component {
+class Navbarnew extends Component {
+
+  onLogoutClick = (e) => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+
   
+
   render() {
     return (
-      <div className="navbar-fixed">
-        <nav className="z-depth-0">
-          <div className="nav-wrapper white">
-            <Link
-              to={this.props.auth.isAuthenticated ? "/dashboard" : "/"}
-              style={{
-                fontFamily: "monospace"
-              }}
-              className="col s5 brand-logo center black-text"
-            >
-              <i className="material-icons">assignment</i>
-              Bukit
-            </Link>
-          </div>
-        </nav>
-      </div>
+      <Navbar className="justify-content-center" bg="dark" variant="dark">
+        <Navbar.Brand
+          href={this.props.auth.isAuthenticated ? "/dashboard" : "/"}
+        >
+          <img
+            alt=""
+            src="https://icons.iconarchive.com/icons/iconsmind/outline/512/Bucket-icon.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          Buckit
+        </Navbar.Brand>
+        {this.props.auth.isAuthenticated ? <Button onClick={this.onLogoutClick} size="lg" variant="primary">
+                Logout
+              </Button> : null}
+      </Navbar>
     );
   }
 }
 
-Navbar.propTypes = {
-  auth: PropTypes.object.isRequired
+Navbarnew.propTypes = {
+  auth: PropTypes.object.isRequired,
+  logoutUser: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({
-  auth: state.auth
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 
-export default connect(mapStateToProps)(Navbar);
+export default connect(mapStateToProps, { logoutUser })(Navbarnew);
